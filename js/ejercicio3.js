c7569827293294d3ac3dec62e07d59c1cef330e4
Abel Almaraz <almarazabeljesus@gmail.com>
2026-04-07 00:09:04 -0300

    estructura ejercicio 1

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -15,24 +15,24 @@ boton.addEventListener("click", () => {
   if (texto === "") return;
 
   // Crear elemento li
-  const li = document.createElement("li");
+  const li = document.createElement("li");//“Creame un elemento HTML de tipo <li>”
   li.className =
     "list-group-item d-flex justify-content-between align-items-center ";
 
-  li.textContent = texto;
+  li.textContent = texto;//agregar cotenido
 
   // Botón eliminar
-  const btnEliminar = document.createElement("button");
-  btnEliminar.textContent = "🗑️";
-  btnEliminar.className = "btn btn-outline-danger btn-sm";
+  const btnEliminar = document.createElement("button");// crea esto <button></button>
+  btnEliminar.textContent = "🗑️";// <button>🗑️</button>
+  btnEliminar.className = "btn btn-outline-danger btn-sm";// da estilo
 
   // Evento eliminar
   btnEliminar.addEventListener("click", () => {
     li.remove();
   });
 
-  li.appendChild(btnEliminar);
-  lista.appendChild(li);
+  li.appendChild(btnEliminar);//Mete el botón dentro del <li>
+  lista.appendChild(li);//Agrega el elemento li dentro de la lista (ul) , (agregar un hijo)
 
   input.value = "";
 });
